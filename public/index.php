8661d8f06306018a48f0f5df8858ed3b707e47a8
joao21dev <joao21dev@gmail.com>
2023-10-09 15:31:02 -0300

    feat: editing contato e pessoa

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 
 use Joao21dev\Magazord\Controller\ExclusaoContato;
 use Joao21dev\Magazord\Controller\ExclusaoPessoa;
+use Joao21dev\Magazord\Controller\FormularioEdicaoContato;
+use Joao21dev\Magazord\Controller\FormularioEdicaoPessoa;
 use Joao21dev\Magazord\Controller\FormularioInsercaoContato;
 use Joao21dev\Magazord\Controller\FormularioInsercaoPessoa;
 use Joao21dev\Magazord\Controller\Home;
@@ -24,6 +26,11 @@ switch (@$_SERVER['PATH_INFO']) {
         $controlador->processaRequisicao();
         break;
 
+    case '/editar-pessoa':
+        $controlador = new FormularioEdicaoPessoa();
+        $controlador->processaRequisicao();
+        break;
+
     case '/excluir-pessoa':
         $controlador = new ExclusaoPessoa();
         $controlador->processaRequisicao();
@@ -36,6 +43,11 @@ switch (@$_SERVER['PATH_INFO']) {
 
     case '/cadastrar-contato':
         $controlador = new FormularioInsercaoContato();
+        $controlador->processaRequisicao();
+        break;
+
+    case '/editar-contato':
+        $controlador = new FormularioEdicaoContato();
         $controlador->processaRequisicao();
         break;
 
